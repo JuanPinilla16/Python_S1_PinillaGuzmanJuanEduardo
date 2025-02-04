45acfb0bd227f413eb71154c185e3a135b95694c
JuanPinilla <juanepinilla16@gmail.com>
2025-02-03 20:18:53 -0500

    Se agrega Trabajo de clase Dia 3

---
+++ Listas_Estudiantes.py
@@ -0,0 +1,86 @@
+nombres = [
+    ["Adrián"],
+    ["Alejandra"],
+    ["Ámbar", "Isabella"],
+    ["Andrés"],
+    ["Camilo", "Andrés"],
+    ["Daniel", "Esteban"],
+    ["David", "Santiago"],
+    ["Edgar", "Leonardo"],
+    ["Gerson", "Steven"],
+    ["Harley", "Yefrey"],
+    ["John", "Stiven"],
+    ["Juan", "David"],
+    ["Juan", "David"],
+    ["Juan", "David"],
+    ["Juan", "Eduardo"],
+    ["Juan", "Fernando"],
+    ["Juan", "Jose"],
+    ["Maria", "Juliana"],
+    ["Mateo", "Roman"],
+    ["Naya", "Zarela"],
+    ["Nicolas"],
+    ["Omar", "Fernando"],
+    ["Santiago"],
+    ["Santiago", "Andrés"],
+    ["Santiago"],
+    ["Sara", "Sofía"],
+    ["Sayara", "Yurley"],
+    ["Sergio", "Andrés"],
+    ["Simón", "Dante"],
+    ["Thomas", "Sebastián"],
+    ["Vladimir"]
+]
+
+apellidos = [
+    ["Quintero", "Pinzón"],
+    ["Pinzón", "Alvarez"],
+    ["Plata", "López"],
+    ["Reyes", "Espinel"],
+    ["Pico", "Araque"],
+    ["Suárez", "Fuentes"],
+    ["Guerrero", "Quintero"],
+    ["Vera", "Mendez"],
+    ["Acevedo", "Arteaga"],
+    ["Chaparro", "Martínez"],
+    ["Cabrales", "Vargas"],
+    ["Negron", "Regalado"],
+    ["Saavedra", "Jaimez"],
+    ["Santoyo", "Jaimes"],
+    ["Vargas", "Soto"],
+    ["Pinilla", "Guzmán"],
+    ["Umaña", "Barragan"],
+    ["Abril", "Roman"],
+    ["Saavedra", "Mejia"],
+    ["Camargo"],
+    ["Lizcano", "Jaimes"],
+    ["Chedraui", "Mantilla"],
+    ["Granados", "Parra"],
+    ["Aguilar", "Vesga"],
+    ["Quiñonez", "Sosa"],
+    ["Jaimes", "Perez"],
+    ["Díaz", "Rodríguez"],
+    ["Aparicio", "Arciniegas"],
+    ["Rueda", "Hernández"],
+    ["Salamanca", "Galvis"],
+    ["Bastos", "Garcia"],
+    ["Diaz", "Contreras"]
+]
+
+for i in range(len(nombres)):
+    print("estudiantes ", i+1, " ",nombres[i], apellidos[i])
+f=True
+while f==True:
+    n=int(input("ingrese el numero del estudiante que quiera editar: "))
+    n=n-1
+    NombreEditado=input("ingrese el numevo nombre para el estudiante: ")
+    ApellidoEditado=input("ingrese el apellido que quiera editar")
+    listaramdom=[NombreEditado]
+    listaramdom2=[ApellidoEditado]
+    nombres[n]=listaramdom
+    apellidos[n]=listaramdom2
+    y=int(input("¿quiere seguir editando? (1=si)//(2=no)"))
+    if y==2:
+        f=False
+for i in range(len(nombres)):
+    print("estudiante " , i+1," ", nombres[i], apellidos[i] )
